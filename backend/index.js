529ddd265e21397619cf81afece23f1f1ae4c278
Kasun Sagara <kasunsagara689@gmail.com>
2025-08-05 22:04:40 +0530

    create database

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 const app = express();
 
+const mongoUrl = "mongodb+srv://kasunsagara689:56649901@cluster0.hulg4fz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
 });
