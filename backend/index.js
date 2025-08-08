acee1479c77727a00ddc639cf29e93aae9991821
Kasun Sagara <kasunsagara689@gmail.com>
2025-08-08 14:27:02 +0530

    database connection done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 
 const app = express();
 
 const mongoUrl = "mongodb+srv://kasunsagara689:56649901@cluster0.hulg4fz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+mongoose.connect(mongoUrl, {})
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("Database connected");
+});
+
+app.use(bodyParser.json());
+
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
 });
