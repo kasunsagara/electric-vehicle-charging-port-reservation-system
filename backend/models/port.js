ac78ba3513d2b6dbbb9618ce36a44b175ec6af07
Kasun Sagara <kasunsagara689@gmail.com>
2025-08-09 13:03:02 +0530

    create user and port models

---
+++ backend/models/port.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+const portSchema = mongoose.Schema({
+  portNumber: {
+     type: Number,
+     required: true,
+   },
+  location: {
+    type: String,
+    required: true,
+  },
+  coordinates: {
+    lat: {
+      type: Number,
+      required: true
+    },
+    lng: {
+      type: Number,
+      required: true
+    }
+  },
+  status: {
+    type: String,
+    enum: ["available", "booked"],
+    default: "available",
+  }
+});
+
+const Port = mongoose.model("ports", portSchema);
+
+export default Port;
---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  phone: {
+    type: String,
+    required: true,
+  }
+});
+
+const User = mongoose.model("users", userSchema);
+
+export default User;
