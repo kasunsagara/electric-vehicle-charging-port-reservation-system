b50576cac21ee5f7c126bf33ee9f40da33a7ca35
Kasun Sagara <kasunsagara689@gmail.com>
2025-08-10 10:49:48 +0530

    password hashing applied

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,14 @@
 import User from "../models/user.js";
+import bcrypt from 'bcrypt';
 
 export async function createUsers(req, res) {
     try {
-        const user = new User(req.body)
-        await user.save()
+        const newUserData = req.body;
+        newUserData.password = bcrypt.hashSync(newUserData.password, 10);
+
+        const user = new User(newUserData);
+        await user.save();
+
         res.json({
             message: "User created successfully"
         });
@@ -14,3 +19,4 @@ export async function createUsers(req, res) {
         });
     }
 }
+
