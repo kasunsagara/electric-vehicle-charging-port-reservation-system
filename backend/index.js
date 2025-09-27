5566eaaf0ad4bec0da012cf34a5f15871846b0fc
Kasun Sagara <kasunsagara689@gmail.com>
2025-09-27 09:36:54 +0530

    CORS was created correctly

--- backend/index.js
+++ backend/index.js
@@ -12,8 +12,19 @@ dotenv.config();
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:8081"
+];
+
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
 
