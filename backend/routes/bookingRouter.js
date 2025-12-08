f9dcd0bc7031dd3dc2b5ae622ad3f5082378a13b
Kasun Sagara <kasunsagara689@gmail.com>
2025-12-09 01:42:39 +0530

    small change

--- backend/routes/bookingRouter.js
+++ backend/routes/bookingRouter.js
@@ -1,10 +1,9 @@
 import express from "express";
-import { createBooking, getBookings, cancelBooking } from "../controllers/bookingController.js";
+import { createBooking, getBookings } from "../controllers/bookingController.js";
 
 const bookingRouter = express.Router();
 
 bookingRouter.post("/", createBooking);
 bookingRouter.get("/", getBookings);
-bookingRouter.delete("/:bookingId", cancelBooking);
 
 export default bookingRouter;
