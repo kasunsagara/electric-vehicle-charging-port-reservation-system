a2772a537d5149b814c63bb24ce769f24736a6a7
Kasun Sagara <kasunsagara689@gmail.com>
2026-01-07 00:02:29 +0530

    small change

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -5,7 +5,6 @@ dotenv.config();
 
 export default function authMiddleware(req, res, next) {
   const token = req.header("Authorization")?.replace("Bearer ", "");
-  console.log(token);
 
   if (token != null) {
     jwt.verify(token, process.env.JWT_SECRET, (error, decoded) => {
