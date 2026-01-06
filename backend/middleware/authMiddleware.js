44633b5438ac186edfc5d27ef1765f051c9dd538
Kasun Sagara <kasunsagara689@gmail.com>
2026-01-06 16:24:54 +0530

    small change

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export default function authMiddleware(req, res, next) {
   const token = req.header("Authorization")?.replace("Bearer ", "");
